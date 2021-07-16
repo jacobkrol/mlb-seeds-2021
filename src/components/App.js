5bf4cbcf17e8af28a118f14848cf2f592cd80b5c
Jacob Krol <phone12.jk@gmail.com>
2021-07-16 00:04:14 -0500

    Upload v1.0.0

---
+++ src/components/App.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { Flipper, Flipped } from 'react-flip-toolkit';
+import alSeedData from '../data/allongseeddata.json';
+import nlSeedData from '../data/nllongseeddata.json';
+import images from '../images';
+
+const Seed = ({id, team}) => (
+    <div className="seed">
+        <p>{id === 1 ? "1st" : id === 2 ? "2nd" : id === 3 ? "3rd" 
+            : id === 4 ? "WC" : id === 5 ? "WC" 
+            : id === 6 ? "Out" : id === 7 ? "Out" : null}</p>
+        <Flipped key={id} flipId={team} stagger={false}>
+            <img src={images[team]} title={team} alt={team} />
+        </Flipped>
+    </div>
+);
+
+function App() {
+    const [day, setDay] = useState(0);
+    const [alSeeds, setAlSeeds] = useState(alSeedData[day].seeds);
+    const [nlSeeds, setNlSeeds] = useState(nlSeedData[day].seeds);
+    const [playing, setPlaying] = useState(false);
+    const [animate, setAnimate] = useState(null);
+    // const [delay, setDelay] = useState(550);
+
+    useEffect(() => {
+        setAlSeeds(alSeedData[day].seeds);
+        setNlSeeds(nlSeedData[day].seeds);
+    }, [day]);
+
+    useEffect(() => {
+        if(day < alSeedData.length-1 && playing) {
+            const animate = setTimeout(() => {
+                setAlSeeds(alSeedData[day].seeds);
+                setNlSeeds(nlSeedData[day].seeds);
+                setDay(day+1);
+            }, 550); //700-400*(day/alSeedData.length)
+            setAnimate(animate);
+        } else {
+            setPlaying(false);
+        }
+
+        return () => {
+            if(animate) clearTimeout(animate);
+        }
+    }, [day, playing]);
+
+    useEffect(() => {
+        if(!playing) clearTimeout(animate);
+    }, [playing]);
+
+    return (
+        <div id="main-container">
+            <h1>2021 Post-Season Seeds</h1>
+            <h2>{alSeedData[day].date}</h2>
+            <div>
+                <div className="league-column">
+                    <h3>A.L.</h3>
+                    <Flipper flipKey={alSeeds.join('')} spring="stiff">
+                        {alSeeds.map((team, i) =>
+                            <Seed key={i} id={i+1} team={team} />
+                        )}
+                    </Flipper>
+                </div>
+                <div className="league-column">
+                    <h3>N.L.</h3>
+                    <Flipper flipKey={nlSeeds.join('')} spring="stiff">
+                        {nlSeeds.map((team, i) =>
+                            <Seed key={i} id={i+1} team={team} />
+                        )}
+                    </Flipper>
+                </div>
+            </div>
+            <div id="source">
+                <a href="https://kroljs.com" target="_blank" rel="noreferrer noopener">kroljs.com/mlb-seeds-2021</a>
+            </div>
+            <div id="control-panel">
+                <input type="button" onClick={() => {setPlaying(false); setDay(0);}} value="FIRST" />
+                <input type="button" onClick={() => day > 0 ? setDay(day-1) : null} value="PREV" />
+                <input type="button" onClick={() => setPlaying(!playing)} value={playing ? "PAUSE" : "PLAY"} />
+                <input type="button" onClick={() => day < alSeedData.length-1 ? setDay(day+1) : null} value="NEXT" />
+                <input type="button" onClick={() => {setPlaying(false); setDay(alSeedData.length-1);}} value="LAST" />
+            </div>
+            <div id="references">
+                <span><p>[1]</p><a href="https://www.baseball-reference.com/" target="_blank" rel="noreferrer noopener">baseball-reference.com</a></span>
+                <span><p>[2]</p><a href="https://www.mlb.com/news/mlb-2020-postseason-tiebreaker-scenarios" target="_blank" rel="noreferrer noopener">mlb.com</a></span>
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/images/index.js
@@ -0,0 +1,65 @@
+import diamondbacks from "./diamondbacks.png";
+import braves from "./braves.png";
+import orioles from "./orioles.png";
+import redsox from "./redsox.png";
+import cubs from "./cubs.png";
+import whitesox from "./whitesox.png";
+import reds from "./reds.png";
+import indians from "./indians.png";
+import rockies from "./rockies.png";
+import tigers from "./tigers.png";
+import astros from "./astros.png";
+import royals from "./royals.png";
+import angels from "./angels.png";
+import dodgers from "./dodgers.png";
+import marlins from "./marlins.png";
+import brewers from "./brewers.png";
+import twins from "./twins.png";
+import mets from "./mets.png";
+import yankees from "./yankees.png";
+import athletics from "./athletics.png";
+import phillies from "./phillies.png";
+import pirates from "./pirates.png";
+import padres from "./padres.png";
+import giants from "./giants.png";
+import mariners from "./mariners.png";
+import cardinals from "./cardinals.png";
+import rays from "./rays.png";
+import rangers from "./rangers.png";
+import bluejays from "./bluejays.png";
+import nationals from "./nationals.png";
+
+const images = {
+    "diamondbacks":diamondbacks,
+    "braves":braves,
+    "orioles":orioles,
+    "redsox":redsox,
+    "cubs":cubs,
+    "whitesox":whitesox,
+    "reds":reds,
+    "indians":indians,
+    "rockies":rockies,
+    "tigers":tigers,
+    "astros":astros,
+    "royals":royals,
+    "angels":angels,
+    "dodgers":dodgers,
+    "marlins":marlins,
+    "brewers":brewers,
+    "twins":twins,
+    "mets":mets,
+    "yankees":yankees,
+    "athletics":athletics,
+    "phillies":phillies,
+    "pirates":pirates,
+    "padres":padres,
+    "giants":giants,
+    "mariners":mariners,
+    "cardinals":cardinals,
+    "rays":rays,
+    "rangers":rangers,
+    "bluejays":bluejays,
+    "nationals":nationals
+};
+
+export default images;
